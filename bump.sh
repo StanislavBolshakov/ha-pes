795abb914cd2b3b755ef0d1a7ddccb2777fae068
Stanislav Bolshakov <st.bolshakov@gmail.com>
2023-04-12 21:33:12 +0300

    feat(pes): adjust directory structure to be HACS compliant

--- bump.sh
+++ bump.sh
@@ -3,8 +3,8 @@
 set -ex
 
 VERSION_STRING='"version": '
-CURR_VERSION=$(awk -F \" '/"version": ".+"/ { print $4; exit; }' pes/manifest.json)
+CURR_VERSION=$(awk -F \" '/"version": ".+"/ { print $4; exit; }' custom_components/pes/manifest.json)
 NEXT_VERSION=$1
 
-sed -i "s/\($VERSION_STRING\).*/\1\"$NEXT_VERSION\",/" pes/manifest.json
+sed -i "s/\($VERSION_STRING\).*/\1\"$NEXT_VERSION\",/" custom_components/pes/manifest.json
 
