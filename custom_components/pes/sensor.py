065d256791181a1ec0082b58ae237efa32b95196
Stanislav Bolshakov <st.bolshakov@gmail.com>
2023-04-12 23:14:28 +0300

    fix(pes): add logic to handle faulty updates

--- custom_components/pes/sensor.py
+++ custom_components/pes/sensor.py
@@ -8,7 +8,6 @@ from homeassistant.helpers import config_validation as cv, entity_platform, serv
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from homeassistant.components.sensor import Entity, PLATFORM_SCHEMA
 from homeassistant.const import CONF_NAME, CONF_TOKEN
-from homeassistant.util import retry
 
 import voluptuous as vol
 import logging
@@ -27,7 +26,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 SCAN_INTERVAL = timedelta(minutes=5)
 
-
 async def async_setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
@@ -35,42 +33,31 @@ async def async_setup_platform(
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up the sensor platform."""
-    tries = 0
-    max_tries = 6
+
     _LOGGER.debug(config)
 
-    while tries < max_tries:
-        try:
-            with async_timeout.timeout(60*10):
-                name = config[CONF_NAME]
-                token = config[CONF_TOKEN]
-                client = Client(token)
-                add_entities([PesSensor(name, client)], update_before_add=True)
+    name = config[CONF_NAME]
+    token = config[CONF_TOKEN]
+    client = Client(token)
+    add_entities([PesSensor(name, client)], update_before_add=False)
 
-                platform = entity_platform.async_get_current_platform()
-                platform.async_register_entity_service(
-                    "indication_raw_updater",
-                    {
-                        vol.Required("peak_value"): cv.positive_int,
-                        vol.Required("offpeak_value"): cv.positive_int,
-                    },
-                    "_send_raw_indication",
-                )
-                platform.async_register_entity_service(
-                    "indication_incremental_updater",
-                    {
-                        vol.Required("peak_value"): cv.positive_int,
-                        vol.Required("offpeak_value"): cv.positive_int,
-                    },
-                    "_send_incremental_indication",
-                )
-                return True
-        except Exception as e:
-            tries += 1
-            if tries == max_tries:
-                _LOGGER.error("Failed to set up custom component after %s tries", tries)
-                return False
-            _LOGGER.warning("Failed to set up custom component on try %s: %s", tries, e)
+    platform = entity_platform.async_get_current_platform()
+    platform.async_register_entity_service(
+        "indication_raw_updater",
+        {
+            vol.Required("peak_value"): cv.positive_int,
+            vol.Required("offpeak_value"): cv.positive_int,
+        },
+        "_send_raw_indication",
+    )
+    platform.async_register_entity_service(
+        "indication_incremental_updater",
+        {
+            vol.Required("peak_value"): cv.positive_int,
+            vol.Required("offpeak_value"): cv.positive_int,
+        },
+        "_send_incremental_indication",
+    )
 
 class PesSensor(Entity):
     def __init__(self, name: str, client: Client):
@@ -103,19 +90,24 @@ class PesSensor(Entity):
 
     def update(self) -> None:
         _LOGGER.debug("Sensor update triggered")
+
         if not self.attrs["account_id"]:
             self.attrs["account_id"] = self.client.get_accounts()[0].get("accountId")
+
         data = self.client.get_account_data(self.attrs["account_id"])
-        balance = data.get("balanceDetails").get("balance")
-        for value in data.get("indicationInfo").get("subServices"):
-            scale = value.get("scale").lower()
-            self.attrs[scale + "_indication"] = value.get("value")
-            self.attrs[scale + "_updated"] = value.get("date")
-        self._attr_native_value = balance
-        self.attrs["balance"] = balance
-        self._state = balance
-        self._available = True
-        _LOGGER.debug(f"Update result: {data}")
+        if data.get('errors'):
+            _LOGGER.error(f"Error occured during sensor update. Details {data.get('errors')}")
+        else:
+            _LOGGER.debug(f"Update result: {data}")
+            balance = data.get("balanceDetails").get("balance")
+            for value in data.get("indicationInfo").get("subServices"):
+                scale = value.get("scale").lower()
+                self.attrs[scale + "_indication"] = value.get("value")
+                self.attrs[scale + "_updated"] = value.get("date")
+            self._attr_native_value = balance
+            self.attrs["balance"] = balance
+            self._state = balance
+            self._available = True
 
     def _send_raw_indication(self, peak_value: int, offpeak_value: int):
         _LOGGER.debug(
